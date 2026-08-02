ab965896ea3cfc67230279199fcb56775d5e3b09
ViSovereign <35148460+ViSovereign@users.noreply.github.com>
2026-08-01 22:30:00 -0400

    Update users.nix
    adding hashedpassword for login

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -3,6 +3,7 @@
     users.users.${args.user} = {
       isNormalUser = true;
       description = "Captain";
+      hashedPassword = "$6$0ghV/lIB43aBa1YX$l/R1EafwHeJXZcO3NxelWrf2xxBjAzrMShK.uHvvlh4DwGBle93CDUyj9t4tckuE2MPG6Dyk.OAOvFwIRNzpk.";
       extraGroups = [
         "networkmanager"
         "wheel"
