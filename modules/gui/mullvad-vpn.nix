a43f378cac7cbc3eadd2f2f57447203148f7ef31
ViSovereign <sovvymobile@gmail.com>
2026-09-05 16:32:39 -0400

    got mullvad config fixed

--- modules/gui/mullvad-vpn.nix
+++ modules/gui/mullvad-vpn.nix
@@ -1,10 +1,10 @@
 {
-  modules.nixos.cli.mullvad-vpn = { pkgs, ... }: {
-    environment.systemPackages = [ pkgs.mullvad-vpn ];
+  modules.nixos.gui.mullvad-vpn = { pkgs, ... }: {
+    environment.systemPackages = [ pkgs.mullvad-vpn pkgs.mullvad];
 
     services.mullvad-vpn = {
+      enable = true;
       gui.enable = true;
-      package = pkgs.mullvad-vpn;
     };
   };
 }
